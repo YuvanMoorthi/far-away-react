47f35dceb9b0eea379ad2154bf88094f0065b256
YuvanMoorthi <yuvanmoorthi34@gmail.com>
2025-06-29 17:03:18 +0530

    Set up GitHub Pages deployment with homepage and scripts

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -12,7 +12,7 @@ export default function Stats({ items }) {
     return (
         <footer className="stats">
             <em>
-                {percentage === 100 ? "You got everything! Ready to go ✈" : ` You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage} %)`}
+                {percentage === 100 ? "You got everything! Ready to go ✈️" : ` You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage} %)`}
             </em>
         </footer>
     );
--- src/components/logo.js
+++ src/components/logo.js
@@ -1,4 +1,4 @@
 export default function Logo() {
-    return <h1>🏝  Far Away 🎁</h1>;
+    return <h1>🏝️ Far Away 🧳</h1>;
 
 }
